aab219cb0b302b0eb60f616ecfebef778ca2ed2f
Kevin Cha <kchaaa@users.noreply.github.com>
2018-05-25 16:29:01 -0700

    Updated rw_classification.py

--- rw_classification.py
+++ rw_classification.py
@@ -29,7 +29,7 @@ from sklearn.metrics import accuracy_score, confusion_matrix, classification_rep
 from sklearn.metrics import roc_curve, roc_auc_score
 
 # Read in data ----
-file_path = 'C:\\Users\\phato_000\\Documents\\ChaKProjects\\rw_project\\processed\\redwine.csv'
+file_path = '..\\processed\\redwine.csv'
 rw_data = pd.read_csv(file_path)
 
 # Prediction Target
